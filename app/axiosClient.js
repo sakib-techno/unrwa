86c7d46fdf08197b5d832343ca9fa72e9b66bee7
Sakib Anjum <sakib@technovicinity.com>
2024-03-22 17:05:20 +0600

    Update axiosClient.js

--- app/axiosClient.js
+++ app/axiosClient.js
@@ -2,16 +2,16 @@
 const {main_url}=process.env;
 import axios from "axios";
 // import { useDispatch, useSelector} from "react-redux";
-import {getCookie} from "cookies-next";
+import { getCookie } from "cookies-next";
 const api_base_url = process.env.NEXT_PUBLIC_API_BASE_URL;
 const axiosClient = axios.create({
-
     baseURL: `${api_base_url}/api/`,
 });
 axiosClient.interceptors.request.use((config) => {
      const token =   getCookie('authToken');
     // const token =   "kazi";
     config.headers.Authorization ="Bearer "+token;
+    config.headers['Access-Control-Allow-Origin'] ="*";
     return config;
 });
 
@@ -26,13 +26,16 @@ axiosClient.interceptors.response.use(
                 //    401 Unauthorized;
                 localStorage.removeItem("ACCESS_TOKEN");
             }
+
             if (response.status === 405) {
                 alert("Method Not Allowed");
             }
+
             return response;
         } catch (e) {
             console.error(e);
         }
+
         throw error;
     }
 );

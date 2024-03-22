ce53a4487fe082f3b2997009775c969cef7df119
Sakib Anjum <sakib@technovicinity.com>
2024-03-22 16:54:30 +0600

    update url

--- next.config.js
+++ next.config.js
@@ -3,7 +3,7 @@ const nextConfig = {
 	reactStrictMode: true,
     images: {
         domains: [
-            "localhost",
+            "https://65fd626c895f7e0008004c70--coruscating-praline-35566f.netlify.app",
         ],
     },
 }
